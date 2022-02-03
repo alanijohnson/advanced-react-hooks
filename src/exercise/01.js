cbeb4f7c816db7a682cb66f72438915e0377a309
Alani Johnson <alani.johnson@smartsheet.com>
2022-02-03 11:45:24 -0600

    Exercise 1. Extra Credit 2 solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -7,17 +7,21 @@ function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
 
- const useStateReducer = (prevState, newState) => {
-   return prevState + newState
+ const countReducer = (prevState, updateState) => {
+   console.log({...prevState} && updateState)
+   return {...prevState} && updateState
  }
 
-  const [count, setCount] = React.useReducer(useStateReducer ,initialCount)
+  const [state, setState] = React.useReducer(countReducer , {
+    count: initialCount
+  })
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two slines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(step)
+  const {count} = state
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
